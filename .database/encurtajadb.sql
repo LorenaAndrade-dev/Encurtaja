23bd7dc3a8239fd1f07720e76f83e6905c0da662
LorenaAndrade-dev <andradesantos.lorena1@gmail.com>
2024-10-09 11:54:49 -0300

    Bug no py

---
+++ .database/encurtajadb.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS encurtajadb;
+
+CREATE DATABASE encurtajadb
+	CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+USE encurtajadb;
+
+CREATE TABLE redir (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    name VARCHAR(127) NOT NULL,
+    link TEXT NOT NULL,
+    short VARCHAR(127) NOT NULL,
+    expire DATETIME DEFAULT NULL,
+    views INT DEFAULT '0',
+    status ENUM('on', 'del')
+);
+
+-- REMOVA-ME após executar pela primeira vez
+DROP TRIGGER IF EXISTS set_expire_before_insert;
